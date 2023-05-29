454f9083bf22fa24a07b4f54ab10a83d92fb2e49
Anon <65692907+ERM073@users.noreply.github.com>
2023-05-29 22:25:54 +0900

    Update proxy-setup.sh

--- proxy-setup.sh
+++ proxy-setup.sh
@@ -8,17 +8,14 @@ apt install privoxy -y
 apt install ufw -y
 
 # Open Proxy Port
-sudo ufw allow 9741
+sudo ufw allow 8118
 sudo ufw reload 
+
 # Add the following lines at the end of the file:
-if ! grep -q "listen-address 0.0.0.0:9741" /etc/privoxy/config; then
-  echo "listen-address 0.0.0.0:9741" | sudo tee -a /etc/privoxy/config
-fi
-
-if ! grep -q "permit-access 0.0.0.0" /etc/privoxy/config; then
-  echo "permit-access 0.0.0.0" | sudo tee -a /etc/privoxy/config
-fi
-
+sudo sed -i '/^listen-address/d' /etc/privoxy/config
+sudo sed -i '/^permit-access/d' /etc/privoxy/config
+echo "listen-address 0.0.0.0:8118" | sudo tee -a /etc/privoxy/config
+echo "permit-accress 0.0.0.0" | sudo tee -a /etc/privoxy/config
 # Save and exit the nano editor
 
 # Restart Privoxy service
@@ -32,5 +29,5 @@ external_ip=$(curl -s https://ipinfo.io/ip)
 # Display server's external IP and Privoxy port
 echo "################################"
 echo "Proxy IP: $external_ip"
-echo "Proxy Port: 9741"
+echo "Proxy Port: 8118"
 echo "################################"
